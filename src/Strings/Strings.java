837447442bb4a99a16ca67da6967921b1c19ab15
GideonOgunleye <gogunleye@ssl247.co.uk>
2017-12-27 16:05:53 +0000

    4th commit

---
+++ src/Strings/Strings.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Hello";
+		String str2 = "World";
+		
+		String Add = str + " " + str2;
+		
+		System.out.println("String Value Is: " + Add);
+		
+		//
+		
+		String Str3 = new String ("Welcome");
+		String Str4 = new String ("Home");
+		
+		
+		System.out.println("Second String Value is: " + Str3 + " " + Str4);
+		
+		
+		//
+
+	}
+
+}
